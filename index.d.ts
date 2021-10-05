b00e8fcfdc6f5ff4b041b6bd03f7fae43ae28e24
Alexander Pushkin <apushkin@appulate.com>
2021-10-05 22:10:38 +0300

    declare types used by exceljs

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,17 @@
 // This type is used in @types/cheerio.
 type Buffer = any;
+
+declare module "stream" {
+	// This type is used in exceljs.
+	type Stream = any;
+}
+
+declare module "events" {
+	// This type is used in exceljs.
+	type EventEmitter = any;
+}
+
+declare namespace NodeJS {
+	// This type is used in exceljs.
+	type TypedArray = any;
+}
